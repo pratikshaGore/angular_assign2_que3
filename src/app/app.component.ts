8c60b13cc832980755a5b46efd656bea20b8c624
pratikshaGore <pratikshagore27@gmail.com>
2018-06-23 00:23:05 +0530

    assignment second 3 que

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>
+  Welcome to {{ title }}!
+</h1>Enter Name:
+<input type="text" placeholder={{name}} name="t1" >`,
+  styles: [`input 
+    {
+        color: blue;
+        font-style: italic;
+        background-color:darkgrey;
+        
+    }`]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Marvellous Infosystems';
+  name="pratu";
 }
